48e1358e0fe8c4eab20dccffdc011a150eab1a4f
Abir Kumar Nandi <116013922+Abir-0007@users.noreply.github.com>
2023-10-09 19:47:59 +0530

    Update Figure.py

--- Visualization Codes/Figure.py
+++ Visualization Codes/Figure.py
@@ -1,22 +1,20 @@
-# import numpy as np 
+import numpy as np 
 from matplotlib import pyplot as plt
 
 
 plt.figure(1)   #first figure
 plt.subplot(211)
-plt.plot([1,2,3])
+plt.plot (np.array([1,2,3]))
 plt.subplot(212)
-plt.plot([4,5,6])
+plt.plot (np.array([4,5,6]))
 # plt.subplot(213)
-plt.plot([7,8,9])
-
-plt.figure(2)   #second figure
-plt.plot([4,5,6])
-
-plt.figure(3) #third figure
-plt.plot([7,8,9])
+plt.plot (np.array([7,8,9]))
 
 plt.figure(1)   #first figure current;
+
+plt.figure(2)   #second figure
+plt.plot (np.array([4,5,6]))
+plt.figure(3)   #first figure current;
 
 plt.subplot(211)    #make subplot(211) in the first figure
 
