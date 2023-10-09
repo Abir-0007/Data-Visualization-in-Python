6f5477de8bd2e76cd3cd9600c73d8275ccf6be35
Abir Kumar Nandi <116013922+Abir-0007@users.noreply.github.com>
2023-10-09 19:28:50 +0530

    Update Bar.py
    I added numpy extension and change the array to numpy.array

--- Visualization Codes/Bar.py
+++ Visualization Codes/Bar.py
@@ -1,9 +1,10 @@
 #code to demonstrate bar graph
+import numpy as np
 import matplotlib.pyplot as plt
 
-x = [5, 2, 9, 4, 7]
-y = [10, 5, 8, 4, 2]
-bar_colors = ['blue', 'green', 'red', 'purple', 'orange']
+x = np.array([5, 2, 9, 4, 7])
+y = np.array([10, 5, 8, 4, 2])
+bar_colors = ['blue', 'yellow', 'red', 'white', 'orange']
 plt.bar(x, y, color=bar_colors)
 
 # Add labels to the axes
